f14b8c179ad382bc9c17183c47d2a074f7607dc6
francorivera <francoriverarivas@hotmail.com>
2022-02-21 19:02:45 -0500

    Initial Commit

---
+++ TurkeyLandmarks/TurkeyLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurkeyLandmarksApp.swift
+//  TurkeyLandmarks
+//
+//  Created by Franco on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TurkeyLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
